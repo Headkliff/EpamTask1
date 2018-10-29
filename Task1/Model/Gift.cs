456604eb5a44fcb247dbc57495d632ecdc25972e
Headkliff <nikitasheiko14@gmail.com>
2018-10-30 01:32:56 +0300

    Adding methods

--- Task1/Model/Gift.cs
+++ Task1/Model/Gift.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace Task1.Model
 {
     public abstract class Gift
     {
-        [StringLength(15, MinimumLength = 5)]
         public string Name { get; }
         public int Weight { get; }
         public List<Candy> Candies;
@@ -20,7 +18,8 @@ namespace Task1.Model
             int currentWeight = 0;
 
             Random random = new Random();
-            int times = random.Next(5, 15);
+            int times = random.Next(5, 20);
+
             for (int i = 0; i < times; i++)
             {
                 int param = random.Next(1, 4);
@@ -29,33 +28,35 @@ namespace Task1.Model
                 if (param == 1)
                 {
                     candy = new Candy("Алёнка", 15, 30, 5);
+                    currentWeight = candy.Weight;
                 }
                 if (param == 2)
                 {
-                    candy = new Candy("Шипучка", 5, 17, 3);
+                    candy = new Candy("Шепучка", 5, 17, 3);
+                    currentWeight = candy.Weight;
                 }
                 if (param == 3)
                 {
                     candy = new Candy("Шоколадка", 20, 120, 10);
+                    currentWeight = candy.Weight;
                 }
                 if (param == 4)
                 {
                     candy = new Candy("Шоколадный батончик", 10, 25, 7);
+                    currentWeight = candy.Weight;
                 }
 
-                if (candy != null)
-                {
-                    currentWeight = candy.Weight;
-                    Candies.Add(candy);
-                }
+
+                Candies.Add(candy);
+                
             }
 
             Weight = currentWeight;
         }
 
-        protected abstract void Open();
+        public abstract void Open();
 
-        public List<Candy> Find(int minValue,int maxValue)
+        public List<Candy> FindCandies(int minValue,int maxValue)
         {
             Open();
 
@@ -64,9 +65,36 @@ namespace Task1.Model
             return candy.ToList();
         }
 
-        public void SortByParam(int param)
+        public List<Candy> SortByParam(List<Candy> candys)
         {
+            Console.Clear();
 
+            Open();
+            Console.WriteLine("Select the option to sort:\n 1-By Name\n 2-By Weight\n 3-By Calories\n 4-By Sugar");
+
+            IOrderedEnumerable<Candy> query;
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    query = candys.OrderBy(item => item.Name);
+                    return query.ToList();
+                    
+                case "2":
+                    query = candys.OrderBy(item => item.Weight);
+                    return query.ToList();
+                    
+                case "3":
+                    query = candys.OrderBy(item => item.Calories);
+                    return query.ToList();
+                    
+                case "4":
+                    query = candys.OrderBy(item => item.Sugar);
+                    return query.ToList();
+
+                default:
+                    return null;
+                    
+            }
         }
     }
 }
